343f9e585edd49bd40ba83886aa1e5eed284ef40
Bibhash Dash <63647219+bibhashdash@users.noreply.github.com>
2022-07-03 23:27:38 +0100

    initial boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <Navbar />
+  <router-view />
 </template>
 
+<script>
+import Navbar from "./components/Navbar.vue";
+export default {
+  components: { Navbar },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -25,6 +28,6 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #0f1271;
 }
 </style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>This is a contact page</h1>
+  <div class="video-with-old-data">
+    <div class="">
+      {{ anyVariable }}
+    </div>
+    <form action="">
+      <input type="text" v-model="name" placeholder="name" />
+    </form>
+    <div class="">
+      <h2>Name is: {{ name }}</h2>
+    </div>
+    <button @click="clickHandler">Update Variable</button>
+  </div>
+
+  <div class="video-with-new-data">
+    <div class="new-video">
+      <video src="">
+        {{ anyVariable }}
+      </video>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    const anyVariable = ref(0);
+    const name = ref("");
+
+    const clickHandler = () => {
+      anyVariable.value++;
+    };
+    return { anyVariable, clickHandler, name };
+  },
+};
+</script>
+
+<style></style>
